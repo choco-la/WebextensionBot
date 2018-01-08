55f62e7f80043e93ceb3f9ad068e911ed1be25a2
hyoro-were <choco-la@users.noreply.github.com>
2018-01-08 17:50:27 +0900

    update functions

--- ts/evalcalc.ts
+++ ts/evalcalc.ts
@@ -1,23 +1,49 @@
 export const evalCalc = (rawinput: string): number => {
-  // tslint:disable-next-line: ter-no-irregular-whitespace
-  const input = replRand(rawinput).replace(/　/g, ' ')
-                                  .replace(/＋/g, '+')
-                                  .replace(/−/g, '-')
-                                  .replace(/xｘ×✕✖/gi, '*')
-                                  .replace(/÷➗/g, '/')
-                                  .replace(/,/g, '')
+  const randomReplaced = replRand(rawinput)
+  const maybeExpression = makeIntoExpression(randomReplaced)
+  const expression = getValidExpression(maybeExpression)
+  if (expression === null) return NaN
 
+  return evaluateAsCalculation(expression)
+}
+
+const replRand = (raw: string): string => {
+  const randomRe = /\$rand(?:om)?/i
+  let result = raw.replace(randomRe, Math.round(Math.random() * 100).toString())
+  while (randomRe.test(result)) {
+    result = result.replace(randomRe, Math.round(Math.random() * 100).toString())
+  }
+  return result
+}
+
+const makeIntoExpression = (rawinput: string): string => {
+  // tslint:disable-next-line: ter-no-irregular-whitespace
+  const expression = rawinput.replace(/　/g, ' ')
+                             .replace(/＋/g, '+')
+                             .replace(/−/g, '-')
+                             .replace(/xｘ×✕✖/gi, '*')
+                             .replace(/÷➗/g, '/')
+                             .replace(/,/g, '')
+
+  return expression
+}
+
+const getValidExpression = (input: string): string | null => {
   // Check if it contains any invalid characters.
-  if (/[^0-9.+\-%^&*/><()\s]/.test(input)) return NaN
+  if (/[^0-9.+\-%^&*/><()\s]/.test(input)) return null
 
   // Separate Numeral or operator symbol.
   const pattern = String.raw`(\-?[0-9.]+|[+\-%^&]|[*/]{1,2}|[><]{2}|[()]+)`
   const re = new RegExp(pattern, 'g')
   const matchs = input.match(re)
   // Return if it is corrupt expression.
-  if (!matchs) return NaN
-  const expression = matchs.join(' ')
+  if (!matchs) return null
 
+  const expression = matchs.join(' ')
+  return expression
+}
+
+const evaluateAsCalculation = (expression: string): number => {
   let result: number
   try {
     // tslint:disable-next-line: no-eval
@@ -31,13 +57,4 @@ export const evalCalc = (rawinput: string): number => {
   // Rounded to 3 digits after the decimal point.
   if (decFraction && decFraction.length > 3) return Number(result.toFixed(3))
   else return result
-}
-
-const replRand = (raw: string): string => {
-  const randomRe = /\$rand(?:om)?/i
-  let result = raw.replace(randomRe, Math.round(Math.random() * 100).toString())
-  while (randomRe.test(result)) {
-    result = result.replace(randomRe, Math.round(Math.random() * 100).toString())
-  }
-  return result
 }
