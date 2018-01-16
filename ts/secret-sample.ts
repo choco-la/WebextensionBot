5f77fa3854ec0691ca0e82e715d6739c9835ab1c
hyoro-were <choco-la@users.noreply.github.com>
2018-01-16 20:43:33 +0900

    change filter array to set

--- ts/secret-sample.ts
+++ ts/secret-sample.ts
@@ -3,4 +3,22 @@ const filterWords = [
   String.raw`殺す`
 ]
 
-export const secretFilter = new RegExp(filterWords.join('|'))
+class RegexFilter {
+  private regexs: Set<RegExp>
+  constructor (patterns: string[]) {
+    this.regexs = new Set()
+    for (const pattern of patterns) {
+      const re = new RegExp(pattern)
+      this.regexs.add(re)
+    }
+  }
+
+  public test (text: string) {
+    for (const re of this.regexs) {
+      if (re.test(text)) return true
+    }
+    return false
+  }
+}
+
+export const secretFilter = new RegexFilter(filterWords)
