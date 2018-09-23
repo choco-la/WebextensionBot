9fdf6aab89a1dfbe8665155b56e4095937939787
hyoro-were <choco-la@users.noreply.github.com>
2018-09-24 02:24:09 +0900

    fix cannot use fetch

--- ts/request.ts
+++ ts/request.ts
@@ -1,6 +1,11 @@
 export type ISender = (url: string, data: string | null) => Promise<Response>
 export type IFetcher = (url: string) => Promise<Response>
 
+interface IContent {
+  fetch: (req: Request) => Promise<Response>
+}
+declare var content: IContent
+
 const post = (url: string, token: string, data: string | null): Promise<Response> => {
   const HTTPHeaders = new Headers()
   HTTPHeaders.append('Content-Type', 'application/json; charset=utf-8')
@@ -11,7 +16,7 @@ const post = (url: string, token: string, data: string | null): Promise<Response
     method: 'POST'
   }
   const request = new Request(url, conf)
-  return fetch(request)
+  return content.fetch(request)
 }
 
 const get = async (url: string, token: string): Promise<Response> => {
@@ -22,7 +27,7 @@ const get = async (url: string, token: string): Promise<Response> => {
     method: 'GET'
   }
   const request = new Request(url, conf)
-  const response = await fetch(request)
+  const response = await content.fetch(request)
   if (!response.ok) throw new Error(response.statusText)
   if (response.status < 200 || response.status >= 300) {
     const error = new Error(response.statusText)
